3508d09d59fda277fe2eb5907a9a6649c6582509
jeremysecondstate <jeremy@secondstate.art>
2026-05-24 18:37:11 -0700

    Install venue-aware Use Mid extension

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from app.ui.polished_theme import install_polished_cockpit_theme
 from app.ui.schwab_live_status_extension import install_schwab_live_status_extension
 from app.ui.trade_setup_extension import install_trade_setup_extension
 from app.ui.trading_cockpit import SchwabTradingCockpitApp
+from app.ui.venue_mid_extension import install_venue_mid_extension
 
 
 def main() -> None:
@@ -19,6 +20,7 @@ def main() -> None:
     install_advanced_actions_extension(SchwabTradingCockpitApp)
     install_hyperliquid_trading_extension(SchwabTradingCockpitApp)
     install_options_lab_extension(SchwabTradingCockpitApp)
+    install_venue_mid_extension(SchwabTradingCockpitApp)
     install_options_core_math_extension()
     install_account_sources_fix(SchwabTradingCockpitApp)
     install_options_resizable_layout_extension()
