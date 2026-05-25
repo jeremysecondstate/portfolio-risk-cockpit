7be0b94c8ba3475a494384775fa2ad0247374563
jeremysecondstate <jeremy@secondstate.art>
2026-05-25 05:27:22 -0700

    Install Schwab workspace sync extension

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from app.ui.options_lab_extension import install_options_lab_extension
 from app.ui.options_resizable_layout_extension import install_options_resizable_layout_extension
 from app.ui.polished_theme import install_polished_cockpit_theme
 from app.ui.schwab_live_status_extension import install_schwab_live_status_extension
+from app.ui.schwab_workspace_sync_extension import install_schwab_workspace_sync_extension
 from app.ui.trade_setup_extension import install_trade_setup_extension
 from app.ui.trading_cockpit import SchwabTradingCockpitApp
 from app.ui.unified_refresh_extension import install_unified_refresh_extension
@@ -28,6 +29,7 @@ def main() -> None:
     install_options_lab_extension(SchwabTradingCockpitApp)
     install_hyperliquid_perp_ticket_use_mid_fix(SchwabTradingCockpitApp)
     install_hyperliquid_existing_perp_what_if_extension(SchwabTradingCockpitApp)
+    install_schwab_workspace_sync_extension(SchwabTradingCockpitApp)
     install_venue_mid_extension(SchwabTradingCockpitApp)
     install_options_core_math_extension()
     install_account_sources_fix(SchwabTradingCockpitApp)
