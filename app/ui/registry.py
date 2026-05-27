f0148bdc880975a5176a7f5016c6e3c82d5ab40b
jeremysecondstate <jeremy@secondstate.art>
2026-05-27 09:43:09 -0700

    Wire Hyperliquid assessment extension

--- app/ui/registry.py
+++ app/ui/registry.py
@@ -8,6 +8,7 @@ from app.ui.account_sources_fix import install_account_sources_fix
 from app.ui.advanced_actions_extension import install_advanced_actions_extension
 from app.ui.cash_positions_extension import install_cash_positions_extension
 from app.ui.company_reports_extension import install_company_reports_extension
+from app.ui.hyperliquid_assessment_extension import install_hyperliquid_assessment_extension
 from app.ui.hyperliquid_cockpit_spot_mid_extension import install_hyperliquid_cockpit_spot_mid_extension
 from app.ui.hyperliquid_existing_perp_what_if_extension import install_hyperliquid_existing_perp_what_if_extension
 from app.ui.hyperliquid_notifications_fix import install_hyperliquid_notifications_fix
@@ -62,5 +63,6 @@ def install_ui_extensions(app_cls: Type[tk.Tk]) -> None:
     install_schwab_output_popout_extension(app_cls)
     install_schwab_live_status_extension(app_cls)
     install_schwab_sync_report_extension(app_cls)
+    install_hyperliquid_assessment_extension(app_cls)
     install_company_reports_extension(app_cls)
     install_unified_trade_thesis_extension(app_cls)
